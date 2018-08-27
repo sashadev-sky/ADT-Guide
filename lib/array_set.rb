6fea3716824286f61af1f42efe2343f39d790999
sashadev-sky <sboginsky17@gmail.com>
2018-08-27 01:53:20 -0400

    Add multiple Set implementations using Array

---
+++ lib/array_set.rb
@@ -0,0 +1,117 @@
+# Modification a:
+
+class MaxIntSet
+  def initialize(max)
+    @store = Array.new(max, false)
+  end
+
+  def insert(num)
+    validate!(num)
+    return false if @store[num]
+    @store[num] = true
+  end
+
+  def remove(num)
+    validate!(num)
+    return nil unless include?(num)
+    @store[num] = false
+    num
+  end
+
+  def include?(num)
+    validate!(num)
+    @store[num]
+  end
+
+  private
+
+  def is_valid?(num)
+    num.between?(0, @store.length - 1)
+  end
+
+  def validate!(num)
+    raise "Out of bounds" unless is_valid?(num)
+  end
+end
+
+# -----------------------------------------------------------
+
+# Modification b:
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    return false if include?(num)
+    self[num] << num
+    num
+  end
+
+  def remove(num)
+    self[num].delete(num)
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+# -----------------------------------------------------------
+
+# Modification c:
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+    return false if include?(num)
+    self[num] << num
+    @count += 1
+    resize! if num_buckets < @count
+
+    num
+  end
+
+  def remove(num)
+    @count -= 1 if self[num].delete(num)
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    old_store = @store
+    @count = 0
+    @store = Array.new(num_buckets * 2) { Array.new }
+
+    old_store.flatten.each { |num| insert(num) }
+  end
+
+  def [](num)
+    @store[num % num_buckets]
+  end
+end
