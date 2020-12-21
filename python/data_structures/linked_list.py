86e48363d09b24f31e2da36ae32505cf6e1b3281
sashadev-sky <sboginsky17@gmail.com>
2020-12-21 00:20:46 -0500

    Extract Node class

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,13 +1,4 @@
-class Node:
-
-    def __init__(self, key=None, val=None, next_node=None, prev_node=None):
-        self.key = key
-        self.val = val
-        self.next = next_node
-        self.prev = prev_node
-
-    def __str__(self):
-        return f"{self.key}: {self.val}"
+from python.data_structures.node import Node
 
 
 class SinglyLinkedList:
