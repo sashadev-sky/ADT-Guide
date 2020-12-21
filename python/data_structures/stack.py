1a02d8e154fb0ab31e8223db69e9288c0c8d5407
sashadev-sky <sboginsky17@gmail.com>
2020-12-21 00:05:02 -0500

    Python stack

---
+++ python/data_structures/stack.py
@@ -0,0 +1,13 @@
+class ArrayStack:
+    def __init__(self):
+        stack_arr = []
+
+    def push(self, el):
+        self.stack_arr.append(el)
+
+    def pop(self):
+        self.stack_arr.pop
+
+    def peek(self):
+        return self.stack_arr.last
+
